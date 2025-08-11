b3079b6ad04217228956395deaa3a5503f9e0a88
Chhavi goel <chhavigoelc15@gmail.com>
2025-08-11 20:39:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+ function showForm() {
+            document.querySelector('.product-list').style.display = 'none';
+            document.querySelector('#form-section').style.display = 'block';
+        }
+        function backToList() {
+            document.querySelector('#form-section').style.display = 'none';
+            document.querySelector('.product-list').style.display = 'block';
+        }
